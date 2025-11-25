6c13c6a9030d7f51f374b4d314ebd24e79312522
Stellaeddy123 <edmilsonalbertomarcostudo@gmail.com>
2025-11-25 17:21:40 +0000

    Item da Loja adicionado

--- app/src/main/java/pt/iade/ei/kudigame/model/StoreItem.kt
+++ app/src/main/java/pt/iade/ei/kudigame/model/StoreItem.kt
@@ -1,2 +1,13 @@
 package pt.iade.ei.kudigame.model
 
+import android.os.Parcelable
+import kotlinx.Parcelize.Parcelize
+
+@parcelize
+data class StoreItem(
+    val id: String,
+    val name: String,
+    val description: String,
+    val price: Double,
+    val imageRestName: String? = null
+) : Parcelable
