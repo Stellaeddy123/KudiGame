c82c74636c3cb46f789fa562802dfb16a3ab1069
Stellaeddy123 <edmilsonalbertomarcostudo@gmail.com>
2025-11-26 12:51:54 +0000

    Store melhorado

--- app/src/main/java/pt/iade/ei/kudigame/model/SampleData.kt
+++ app/src/main/java/pt/iade/ei/kudigame/model/SampleData.kt
@@ -1,4 +1,54 @@
 package pt.iade.ei.kudigame.model
 
-class SampleData {
+import android.app.GameState
+import android.icu.text.CaseMap
+import pt.iade.ei.kudigame.ui.theme.components.GameCard
+
+
+object SampleData {
+    val games: List<Game> = listOf(
+        Game(
+            id = "g1",
+            title = "Diablo® IV",
+            genre = "Action / RPG",
+            shortDescription = "Participa na luta por Santuário em Diablo® IV.",
+            imageResName = "Diablo® IV", // coloca arquivo em drawable
+            storeItems = listOf(
+                StoreItem("c1",
+                    "Diablo® IV: Vessel of Hatred™",
+                    "Desvenda o próximo capítulo de Diablo® IV em Vessel of Hatred™",
+                    13.99,
+                    "icon_speed"),
+                StoreItem("c2",
+                    "Diablo® IV - Banda Sonora",
+                    "Recorda a eterna batalha entre o Paraíso Celestia",
+                    10.99, "icon_helmet"),
+                StoreItem("c3",
+                    "Diablo® IV - Pacote de Platina Tesouro do Rebelde",
+                    "Os caçadores de tesouros de Nahantu encontram ",
+                    6.99, "icon_map")
+            )
+        ),
+        Game(
+            id = "g2",
+            title = "Warhammer 40,000: Space Marine 2",
+            genre = "Ação, Aventura, RPG",
+            shortDescription = "Embody the superhuman skill and brutality of a Space Marine.",
+            imageResName = "Warhamer 40,000: Space Marine 2",
+            storeItems = listOf(
+                StoreItem("m1",
+                    "Warhammer 40,000: Space Marine 2",
+                    "Space Marine chapters through four phases", 29.99,
+                    "icon_seed"),
+                StoreItem("m2",
+                    "Warhammer 40,000: Space Marine 2 - Imperial Fists Cosmetic Pack",
+                    "Honour your Chapter's proud martial heritage with the Imperial Fists Cosmetic Pack",
+                    4.99, "icon_barn"),
+                StoreItem("m3",
+                    "Warhammer 40,000: Space Marine 2 - Blood Angels Champion Pack",
+                    "Gameplay consists of frequent combat scenarios",
+                    7.99, "icon_harvest")
+            )
+        )
+    )
 }
--- app/src/main/java/pt/iade/ei/kudigame/ui/theme/components/GameCard.kt
+++ app/src/main/java/pt/iade/ei/kudigame/ui/theme/components/GameCard.kt
@@ -1,2 +1,9 @@
 package pt.iade.ei.kudigame.ui.theme.components
 
+data class GameCard(
+    val id: String,
+    val title: String,
+    val genre: String,
+    val shortDescription: String,
+    val imageResName: String? = null,
+    val storeItem: List<StoreItem> = emptyList()
