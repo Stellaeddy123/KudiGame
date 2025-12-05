2c42b911efc299fb7f86e33fd79b291da9d0099f
Stellaeddy123 <edmilsonalbertomarcostudo@gmail.com>
2025-12-05 11:55:28 +0000

    Adicionei o Bottom do profile, history, e featured

--- app/src/main/java/pt/iade/ei/kudigame/ui/theme/screens/MainActivity.kt
+++ app/src/main/java/pt/iade/ei/kudigame/ui/theme/screens/MainActivity.kt
@@ -14,7 +14,10 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.*
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,10 +28,10 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import pt.iade.ei.kudigame.R
 import pt.iade.ei.kudigame.model.Game
 import pt.iade.ei.kudigame.model.SampleData
 import pt.iade.ei.kudigame.ui.theme.components.GameCard
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,58 +49,97 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScreen (games: List<Game>, onGameClick: (Game) -> Unit) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFFF6EDF7)) //
-            .padding(16.dp)
+fun MainScreen(games: List<Game>, onGameClick: (Game) -> Unit) {
 
-    ) {
-        //
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = "Gudi Game",
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = Color(0xFFF6EDF7)
+                ),
+                title = {
+                    Text(
+                        text = "Kudi Game",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                actions = {
+                    Icon(
+                        imageVector = Icons.Default.Notifications,
+                        contentDescription = "",
+                        modifier = Modifier.padding(horizontal = 12.dp)
+                    )
+                    Icon(
+                        imageVector = Icons.Default.Settings,
+                        contentDescription = ""
+                    )
+                }
             )
+        },
 
-            Row {
-                Icon(
-                    imageVector = Icons.Default.Notifications,
-                    contentDescription = null,
-                    modifier = Modifier.padding(horizontal = 12.dp)
-                )
-                Icon(
-                    imageVector = Icons.Default.Settings,
-                    contentDescription = null
-                )
+        bottomBar = {
+            BottomAppBar(
+                containerColor = Color(0xFFF6EDF7),
+                tonalElevation = 8.dp
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 24.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Column (horizontalAlignment = Alignment.CenterHorizontally) {
+                        Icon(
+                            Icons.Outlined.Star,
+                            contentDescription = "Featured"
+                        )
+                        Text("Featured")
+                    }
+                    Column (horizontalAlignment = Alignment.CenterHorizontally) {
+                        Icon(
+                            painter = painterResource(R.drawable.outline_archive_24),
+                            contentDescription = "History"
+                        )
+                        Text("History")
+                    }
+                    Column (horizontalAlignment = Alignment.CenterHorizontally) {
+                        Icon(
+                            Icons.Outlined.Person,
+                            contentDescription = "Profile"
+                        )
+                        Text("Profile")
+                    }
+
+
+
+                }
             }
         }
+    ) { innerPadding ->
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-        //
-        LazyColumn {
+        LazyColumn(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .background(Color(0xFFF6EDF7))
+                .padding(16.dp)
+        ) {
             items(games) { game ->
                 GameCard(
                     game = game,
                     onClick = { onGameClick(game) }
                 )
                 Spacer(modifier = Modifier.height(16.dp))
-
             }
         }
     }
 }
 
 @Composable
-fun GameCard(game: Game, onGameClick: (Game) -> Unit) {
+fun GameCard(game: Game, onClick: () -> Unit) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -106,7 +148,7 @@ fun GameCard(game: Game, onGameClick: (Game) -> Unit) {
         elevation = CardDefaults.cardElevation(2.dp)
     ) {
 
-        //
+        // IMAGEM COMO FUNDO DO CARD — igual à imagem
         Image(
             painter = painterResource(id = game.imageRestName),
             contentDescription = null,
@@ -114,7 +156,7 @@ fun GameCard(game: Game, onGameClick: (Game) -> Unit) {
             modifier = Modifier.fillMaxSize()
         )
 
-        //
+        // NOME NA PARTE DE BAIXO
         Box(
             modifier = Modifier
                 .fillMaxSize()
@@ -130,8 +172,11 @@ fun GameCard(game: Game, onGameClick: (Game) -> Unit) {
         }
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun PreviewMainScreen() {
-    MainScreen(games = SampleData.games, onGameClick = { })
+    MainScreen(games = SampleData.games, onGameClick = {})
 }
+
+
